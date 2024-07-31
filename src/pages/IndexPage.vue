11537fe23bdf6ed18a1200c16da396caf22503bd
therealindy <kawinslowly@gmail.com>
2024-07-31 11:16:12 +0700

    modified reset() method

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -98,6 +98,8 @@ export default {
       },
 
       onReset () {
+        id.value = null
+        surname.value = null
         name.value = null
         age.value = null
         accept.value = false
